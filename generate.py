e610d30566668045a46debcb8fc83fa068a8dcb2
kludda <david.alind@gmail.com>
2025-11-26 15:36:13 +0100

    Working POC

--- generate.py
+++ generate.py
@@ -118,11 +118,169 @@ if __name__ == "__main__":
             raise ValueError("Missing required gyroid conf: 'periodicity' or 'strut param'")
 
         logger.info("Generating gyroid...")
-        vol = tpms.gyroid.get_voxel_grid(t=t, a=a, res=res, size=size)
+        vol = tpms.gyroid.get_gyroid(t=t, a=a, res=res, size=size)
 
 
     if vol is None:
         raise ValueError("No geometry defined.")
+
+
+    distance = 1
+    #mgm = 0.5
+    p = conf['mesh']['lid pad'] = round(distance / sizeunit_per_voxel)
+    #shift = shift + p * sizeunit_per_voxel
+
+
+    mgm = conf['mesh']['mean_gradient_magnitude'] = tpms.mesh.mean_gradient_magnitude(vol, sizeunit_per_voxel)
+
+
+    x, y, z = tpms.gyroid.get_voxel_grid(res=res, size=size)
+    R = size[0] / size.max() #1
+
+
+
+
+    vola = tpms.mesh.voxel_offset(vol=vol, distance=(distance / 2) * mgm)
+    volb = tpms.mesh.voxel_offset(vol=vol, distance=-(distance / 2) * mgm)
+
+
+
+    '''
+    ca = (x**2 + y**2) <= (R-0.1)**2
+    #ca = (x**2 + y**2) <= R**2
+    #cb = (x**2 + y**2) <= (R-0.1)**2
+    cb = (x**2 + y**2) <= R**2    
+
+    vola[~ca] = 0     # push outside region far above iso-surface
+    volb[~cb] = -2    # push outside region far above iso-surface
+    '''
+
+    #size[0] / size.max()
+
+
+
+    z_max = z >= size[2] / size.max() #1
+    z_min= z <= -size[2] / size.max() #1
+    vola[z_max] -= 2
+    vola[z_min] -= 2
+
+
+    ca = (x**2 + y**2)**0.5 - R
+    #ca = (x**2 + y**2)**0.5 - R + (distance/size.max()) * 2
+    cb = (x**2 + y**2)**0.5 - R + (distance/size.max()) * 2 # space is -1 to 1 = 2 wide
+    #cb = (x**2 + y**2)**0.5 - R
+
+    '''
+    def smin(a, b, k):
+        # smooth min: k controls smoothness
+        return -np.log(np.exp(-k*a) + np.exp(-k*b)) / k
+    '''
+        
+    def smax(a, b, k):
+        return np.log(np.exp(k*a) + np.exp(k*b)) / k
+
+    '''
+    def smin_poly(a, b, k):
+        h = np.clip(0.5 + 0.5*(b - a)/k, 0.0, 1.0)
+        return b*(1-h) + a*h - k*h*(1-h)
+
+    def smax_poly(d1, d2, k):
+        h = np.clip(0.5 + 0.5 * (d1 - d2) / k, 0.0, 1.0)
+        return d1 * h + d2 * (1 - h) + k * h * (1 - h)
+    '''
+    
+    k = 8.0   # higher = sharper; 5–20 is typical
+    vola = smax(vola, ca, k)
+    volb = smax(volb, cb, k)
+
+    #k = 0.01  # smooth blending radius
+    #vola = smax_poly(vola, ca, k)
+    #volb = smax_poly(volb, cb, k)
+
+    #def smoothstep(edge0, edge1, x):
+        #t = np.clip((x - edge0) / (edge1 - edge0), 0, 1)
+        #return t*t*(3 - 2*t)
+
+
+    #w = 0.05       # smoothing width
+    #ca = 1.0 - smoothstep(R - w, R + w, d)
+
+
+    #vola = np.maximum(vola, ca)
+    #volb = np.maximum(volb, cb)
+
+    vola = np.pad(vola, (
+        (0,0),
+        (0,0),
+        (2,2)
+        ),
+        #mode='constant', constant_values=-1.0)
+        mode='edge')
+
+
+
+    vola = np.pad(vola, (
+        (0,0),
+        (0,0),
+        (2,2)
+        ),
+        mode='constant', constant_values=+1.0)
+        #mode='constant', constant_values=0)
+
+    
+
+    volb = np.pad(volb, (
+        (0,0),
+        (0,0),
+        (4,4)
+        ),
+        #mode='constant', constant_values=-10.0)
+        mode='edge')
+        #mode='constant')
+
+    '''
+    volb = np.pad(volb, (
+        (0,0),
+        (0,0),
+        (2,2)
+        ),
+        mode='constant', constant_values=-1.0)
+        #mode='constant', constant_values=0)
+    '''
+
+    '''
+    c = (x**2 + y**2)**0.5 - R
+    ca = np.pad(c, (
+        (0,0),
+        (0,0),
+        #(2,2)
+        (4,4)
+        ),
+        #mode='constant', constant_values=-1)
+        mode='edge')
+    c = (x**2 + y**2)**0.5 - R-0.1
+    cb = np.pad(c, (
+        (0,0),
+        (0,0),
+        #(2,2)
+        (4,4)
+        ),
+        mode='constant', constant_values=-1)
+        #mode='edge')
+    vola = np.maximum(vola, ca)
+    volb = np.maximum(volb, -cb)
+    '''
+
+    #vola = tpms.mesh.voxel_offset(vol=vol, distance=(distance / 2) * mgm)
+    #volb = tpms.mesh.voxel_offset(vol=vol, distance=-(distance / 2) * mgm)
+    
+    vol = np.maximum(vola,-volb)
+    #vol = np.maximum(vola,volb)
+    #vol = np.maximum(-vola,volb)
+    #vol = vola
+    vol, cap_shift = tpms.mesh.voxel_cap_extremes(vol, spacing=sizeunit_per_voxel)
+    
+
 
 
     try:
@@ -133,6 +291,7 @@ if __name__ == "__main__":
         logger.info("Offsetting...")
         mgm = conf['mesh']['mean_gradient_magnitude'] = tpms.mesh.mean_gradient_magnitude(vol, sizeunit_per_voxel)
         vol = tpms.mesh.voxel_offset(vol=vol, distance=distance * mgm, direction='sym')
+
 
 
     try:
